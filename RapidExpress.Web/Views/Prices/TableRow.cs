3913564d6b23a565775e4cae1a86de92dc3bcb95
Krasimir Angelov <kangelov@david.bg>
2021-03-30 01:25:50 +0300

    Modify the "Home" and "Prices" pages markup.

---
+++ RapidExpress.Web/Views/Prices/TableRow.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.Localization;
+
+namespace RapidExpress.Web.Views.Prices
+{
+	public class TableRow
+	{
+		public LocalizedHtmlString StartPoint { get; set; }
+
+		public LocalizedHtmlString EndPoint { get; set; }
+
+		public int Distance { get; set; }
+
+		public LocalizedHtmlString TravelTime { get; set; }
+	}
+}
